72626ea2b82a8d98aec97343c69f0d7b7b9bd1f4
electrovladyslav <electrovladyslav@gmail.com>
2016-08-09 21:19:07 +0400

    Scrollspy affix menu and panels with tech data were added. Some changes was made in table.js

--- js/table.js
+++ js/table.js
@@ -1,31 +1,55 @@
+"use strict";
+
+var scheduleBody = document.querySelector('.schedule-body');
 var ports = document.querySelectorAll('.port');
-var counties = document.querySelectorAll('.county');
+
 var arrivals = document.querySelectorAll('.arrival time');
 var departures = 
 	document.querySelectorAll('.departure time');
 
-var progressBar = 
-	document.querySelector('.progress-bar');
+var now = new Date(); //2016, 2, 4
 
-var arrivalDate =  
-	Date.parse(arrivals[0].attributes[0].value);
-var departureDate = 
-	Date.parse(departures[1].attributes[0].value);
-var now = new Date();
+for (var i = 0; i < arrivals.length; i++) {
+	var arrivalDate =  
+		new Date(arrivals[i].attributes[0].value);
+	var departureDate = 
+		new Date(departures[i].attributes[0].value);
+	var prevDepartureDate = 
+		new Date(departures[i+1].attributes[0].value);
+	
+	if (now > departureDate) { //судно за пределами таблицы
+		ports[i].classList.add('leaved');
+		break;
+	}
 
-if (arrivalDate > departureDate) {
-	var finishDate = arrivalDate;
-	var startDate = departureDate;
-} else {
-	var finishDate = departureDate;
-	var startDate = arrivalDate;
+	if (now >= arrivalDate) {	//судно в порту
+		showProgress (i, "Стоянка в порту", arrivalDate, departureDate);
+		break;
+	}
+
+	if (now >= prevDepartureDate) { //судно на переходе
+		showProgress (i, "Морской переход", prevDepartureDate, arrivalDate);
+		break;
+	};
+};
+//пометим все порты, начиная со следующего, посещёнными
+for (++i; i < ports.length; i++) {
+	ports[i].classList.add('leaved');
+};
+
+function showProgress (i, position, startDate, finishDate) {
+	var voyageProgress 
+		=Math.round( (1 - (finishDate - now)/(finishDate - startDate)) * 100 );
+
+	voyageProgress = (voyageProgress > 100) ? 100 : voyageProgress;
+	voyageProgress = (voyageProgress < 0) ? 0 : voyageProgress;
+	var progressBarMin = (voyageProgress < 10) ? 10 : voyageProgress;
+
+	//progressBar.innerHTML = voyageProgress + "%";
+	var rowProgress = document.createElement('tr');
+	rowProgress.className = 'row-progress';
+	rowProgress.innerHTML = '<td>' + position + '</td><td colspan="4"><div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="' + voyageProgress + '" aria-valuemin="0" aria-valuemax="100" style="width:' + progressBarMin + '%;">' + voyageProgress + '%</div></td>'
+
+	scheduleBody.insertBefore(rowProgress, arrivals[i].parentNode.parentNode.nextElementSibling)
+
 }
-
-var voyageProgress 
-	=Math.round( (1 - (finishDate - now)/(finishDate - startDate)) * 100 );
-
-voyageProgress = (voyageProgress > 100) ? 100 : voyageProgress;
-voyageProgress = (voyageProgress < 0) ? 0 : voyageProgress;
-
-progressBar.style.width = (voyageProgress > 10) ? (voyageProgress + "%") : "10%";
-progressBar.innerHTML = voyageProgress + "%";
